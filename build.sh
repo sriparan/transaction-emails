caf075d0df50e06032b8213e402ad909ef4df5ef
sriparan <19173503+sriparan@users.noreply.github.com>
2021-11-23 14:40:59 -0800

    code pipeline build

--- build.sh
+++ build.sh
@@ -6,19 +6,20 @@ python --version
 
 ## install npm
 NODE_VERSION=16
-sudo yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_${NODE_VERSION}.x | sudo -E bash -
-sudo yum install -y nodejs 
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_${NODE_VERSION}.x | bash -
+yum install -y nodejs 
+
+echo "Node version"
 node -v
+
+echo "NPM Version"
 npm -v
 
-
+echo "Install aws cdk"
+cdk --version
 npm install -g aws-cdk
 cdk --version
-
-
-
-
 
 echo $SRC_TOP
 cd ${SRC_TOP}/activities/stream_consumer
