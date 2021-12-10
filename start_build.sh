f991d6b69025754df4637e99dde449f4f6650c36
sriparan <19173503+sriparan@users.noreply.github.com>
2021-12-09 17:41:18 -0800

    Demo change adding phone number

--- start_build.sh
+++ start_build.sh
@@ -1,2 +1,4 @@
+git commit -a -m "Demo change adding phone number"
 git push
-aws codepipeline list-pipeline-executions --pipeline-name transaction-emails --max-items 2
+sleep 10
+aws codepipeline list-pipeline-executions --pipeline-name transaction-emails --max-items 1 --query "pipelineExecutionSummaries[0].[pipelineExecutionId, status,lastUpdateTime]"
